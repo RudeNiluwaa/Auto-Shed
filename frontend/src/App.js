3fcc673887e7976eaf7df6e9811a0553ebb0ac70
vinuvi-thalagala <vinuvithalagala01@outlook.com>
2025-03-24 10:09:46 +0530

    add path to AddSchedules and GetSchedules

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
+
+//Pamada Pages
 import AddExaminer from './Componenets/AddExaminer';
 import GetExaminers from './Componenets/GetExaminers';
+
+//Vinuvi pages
+import AddRechedule from './Componenets/AddSchedule';
+import GetSchedules from './Componenets/GetSchedules';
 
 function App() {
   return (
@@ -10,8 +16,16 @@ function App() {
     <BrowserRouter>
     
     <Routes>
+      {/* Pamada Roures */}
       <Route path='/add-examiner' element={<AddExaminer />} />
       <Route path='/get-examiner' element={<GetExaminers />} />
+
+      {/* Vinuvi Routes */}
+      <Route path='/add-reschedule' element={<AddRechedule />} />
+      <Route path='/get-reschedule' element={<GetSchedules />} />
+
+
+      
     </Routes>
     </BrowserRouter>
      
